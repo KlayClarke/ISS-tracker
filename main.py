7f4db56a132bdd07a2c40f5024036baef7e52c03
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 15:38:24 -0500

    access api data as json

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import requests
 
-requests.get(url='http://api.open-notify.org/iss-now.json')
+response = requests.get(url='http://api.open-notify.org/iss-now.json')
+response.raise_for_status()
+
+data = response.json()
+
+print(data)
