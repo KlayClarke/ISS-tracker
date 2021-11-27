252cb7b5f1defc8eef97a47977aa84ce12774368
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 15:40:45 -0500

    save iss coordinates (longitude and latitude) as variables

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import requests
 response = requests.get(url='http://api.open-notify.org/iss-now.json')
 response.raise_for_status()
 
-data = response.json()
+positional_data = response.json()['iss_position']
 
-print(data)
+longitude = positional_data['longitude']
+latitude = positional_data['latitude']
+
+print(longitude)
+print(latitude)
